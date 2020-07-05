936dd3238d67d2bd572f506f93bb4d0343e1344e
Sergii Udovichenko <sergii.udovichenko@gmail.com>
2020-07-05 21:20:56 +0300

    Added forms users

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React from "react";
+import "./styles.css";
 
 const App = () => {
-  const message = "This is my first variable rendered in JSX!";  
+  return (
+    <>
+      <ContactCard
+        avatar="https://via.placeholder.com/150"
+        name="Jenny Han"
+        email="jenny.han@notreal.com"
+        age={25}
+      />
+      <ContactCard
+        avatar="https://via.placeholder.com/150"
+        name="Jason Long"
+        email="jason.long@notreal.com"
+        age={45}
+      />
+      <ContactCard
+        avatar="https://via.placeholder.com/150"
+        name="Peter Pan"
+        email="peter.pan@neverland.com"
+        age={100}
+      />
+    </>
+  );
+};
 
-  const handleClick = () =>{
-  alert("you clicked the message!");
-  }
+const ContactCard = props => {
+  const [showAge, setShowAge] = React.useState(false);
 
   return (
-    <div>
-      <h1>Hello React World</h1>
-      <h2 onClick={handleClick}>{message}</h2>
+    <div className="contact-card">
+      <img src={props.avatar} alt="profile" />
+      <div className="user-details">
+        <p>Name: {props.name}</p>
+        <p>Email: {props.email}</p>
+        <button onClick={() => setShowAge(!showAge)}>
+      Toggle Age 
+    </button>
+        {showAge && <p>Age: {props.age}</p>}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
