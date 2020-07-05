0f7eda1a8ddbecb5804f8af7998fa3c69f2991db
Sergii Udovichenko <sergii.udovichenko@gmail.com>
2020-07-05 22:10:01 +0300

    Added data api

--- src/App.js
+++ src/App.js
@@ -2,29 +2,29 @@ import React from "react";
 import "./styles.css";
 
 const App = () => {
+  const [contacts, setContacts] = React.useState([]);
+
+fetch("https://randomuser.me/api/?results=3")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    setContacts(data.results);
+  });
+
   return (
     <>
-      <ContactCard
-        avatar="https://via.placeholder.com/150"
-        name="Jenny Han"
-        email="jenny.han@notreal.com"
-        age={25}
-      />
-      <ContactCard
-        avatar="https://via.placeholder.com/150"
-        name="Jason Long"
-        email="jason.long@notreal.com"
-        age={45}
-      />
-      <ContactCard
-        avatar="https://via.placeholder.com/150"
-        name="Peter Pan"
-        email="peter.pan@neverland.com"
-        age={100}
-      />
+      {contacts.map(contact => (
+        <ContactCard
+          avatar={contact.picture.large}
+          name={contact.name.first + " " + contact.name.last}
+          email={contact.email}
+          age={contact.dob.age}
+        />
+      ))}
     </>
   );
 };
+
 
 const ContactCard = props => {
   const [showAge, setShowAge] = React.useState(false);
