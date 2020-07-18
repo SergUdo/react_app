9e5ed2f8f52815262d847533a3a858bd18fb5bc7
Sergii Udovichenko <sergii.udovichenko@gmail.com>
2020-07-18 21:59:31 +0300

    Updates

---
+++ precache-manifest.083eb21d4c746326a0d9cf382247a4bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5073f037398e274455229669ecdf014",
+    "url": "/react_app/index.html"
+  },
+  {
+    "revision": "b0de2c67ebb6b48c2ced",
+    "url": "/react_app/static/css/main.df61c42f.chunk.css"
+  },
+  {
+    "revision": "8f4183f2e1030af8e122",
+    "url": "/react_app/static/js/2.14540c44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_app/static/js/2.14540c44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0de2c67ebb6b48c2ced",
+    "url": "/react_app/static/js/main.096aa5cf.chunk.js"
+  },
+  {
+    "revision": "5a8afefb4d786908b253",
+    "url": "/react_app/static/js/runtime-main.6618451d.js"
+  }
+]);
